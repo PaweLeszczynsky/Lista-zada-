7e67f4bac655151fd83a4a5992f84e42986a07ca
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-07-20 21:56:16 +0200

    add show list items section function, newPosition changed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 {
 
     const tasks = [
-        
+
     ];
 
     renderTaskList = () => {
-        
-        let newPosition="";
+
+        let newPosition = "";
         for (const task of tasks) {
             newPosition += `
-            <li class="mainSection__section__taskListItem">${task.content}</li>
+            <li class="mainSection__section__taskListItem"><button class="mainSection__section__taskList__checkedButton"><ion-icon class="mainSection__section__taskList__icon" name="checkmark-circle-outline"></ion-icon></button>${task.content}<button class="mainSection__section__taskList__deleteTask"><ion-icon class="mainSection__section__taskList__icon" name="trash-outline"></ion-icon></button></li>
             `;
         }
         document.querySelector(".js-mainSection__section__taskList").innerHTML = newPosition;
+        showTaskListSection();
     }
 
     const addNewTask = (newTask) => {
@@ -30,13 +31,26 @@
         if (newTask === "") {
             return;
         }
-        addNewTask(newTask); 
+        addNewTask(newTask);
+    }
+
+    const showTaskListSection = () => {
+        const taskListSection = document.querySelector(".js-mainSection__taskListSection");
+        if (tasks.length === 0) {
+
+            taskListSection.style.display = "none";
+
+        } else taskListSection.style.display = "flex";
+
+
     }
 
     const init = () => {
-        renderTaskList();
+        // renderTaskList();
+        showTaskListSection();
         const form = document.querySelector(".js-mainSection__section__addTaskForm");
         form.addEventListener("submit", onFormSubmit);
+
 
     };
     init();

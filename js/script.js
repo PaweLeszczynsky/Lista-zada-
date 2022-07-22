33b90545833c609b86dfe8b9ec8d6045d558abe2
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-07-22 22:33:55 +0200

    added changing task status

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
         let newPosition = "";
         for (const task of tasks) {
             newPosition += `
-            <li class="mainSection__section__taskListItem"><button class="mainSection__section__taskList__checkedButton"><ion-icon class="mainSection__section__taskList__icon" name="checkmark-outline"></ion-icon></button>${task.content}<button class="mainSection__section__taskList__deleteTask js-mainSection__section__taskList__deleteTask"><ion-icon class="mainSection__section__taskList__icon" name="trash-outline"></ion-icon></button></li>
+            <li ${task.status ? "style=\"text-decoration: line-through\"" : ""} class="mainSection__section__taskListItem"><button class="mainSection__section__taskList__checkedButton js-mainSection__section__taskList__checkedButton">${task.status ? `<ion-icon class="mainSection__section__taskList__icon" name="checkmark-outline"></ion-icon>` : ""}</button>${task.content}<button class="mainSection__section__taskList__deleteTask js-mainSection__section__taskList__deleteTask"><ion-icon class="mainSection__section__taskList__icon" name="trash-outline"></ion-icon></button></li>
             `;
         }
         document.querySelector(".js-mainSection__section__taskList").innerHTML = newPosition;
@@ -16,6 +16,13 @@
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 tasks.splice(index, 1)
+                renderTaskList();
+            });
+        });
+        const statusTaskButtons = document.querySelectorAll(".js-mainSection__section__taskList__checkedButton");
+        statusTaskButtons.forEach((statusTaskButton, index) => {
+            statusTaskButton.addEventListener("click", () => {
+                tasks[index].status = !tasks[index].status;
                 renderTaskList();
             });
         });

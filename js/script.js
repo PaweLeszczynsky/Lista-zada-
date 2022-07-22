e2075198a44240e43bca8cb26df629cb075dd866
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-07-22 20:03:28 +0200

    deleted not used function nad changed icon

--- js/script.js
+++ js/script.js
@@ -3,17 +3,15 @@
     const tasks = [
 
     ];
-
     renderTaskList = () => {
 
         let newPosition = "";
         for (const task of tasks) {
             newPosition += `
-            <li class="mainSection__section__taskListItem"><button class="mainSection__section__taskList__checkedButton"><ion-icon class="mainSection__section__taskList__icon" name="checkmark-circle-outline"></ion-icon></button>${task.content}<button class="mainSection__section__taskList__deleteTask"><ion-icon class="mainSection__section__taskList__icon" name="trash-outline"></ion-icon></button></li>
+            <li class="mainSection__section__taskListItem"><button class="mainSection__section__taskList__checkedButton"><ion-icon class="mainSection__section__taskList__icon" name="checkmark-outline"></ion-icon></button>${task.content}<button class="mainSection__section__taskList__deleteTask"><ion-icon class="mainSection__section__taskList__icon" name="trash-outline"></ion-icon></button></li>
             `;
         }
         document.querySelector(".js-mainSection__section__taskList").innerHTML = newPosition;
-        showTaskListSection();
     }
 
     const addNewTask = (newTask) => {
@@ -34,20 +32,7 @@
         addNewTask(newTask);
     }
 
-    const showTaskListSection = () => {
-        const taskListSection = document.querySelector(".js-mainSection__taskListSection");
-        if (tasks.length === 0) {
-
-            taskListSection.style.display = "none";
-
-        } else taskListSection.style.display = "flex";
-
-
-    }
-
     const init = () => {
-        // renderTaskList();
-        showTaskListSection();
         const form = document.querySelector(".js-mainSection__section__addTaskForm");
         form.addEventListener("submit", onFormSubmit);
 

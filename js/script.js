be6652c37b91182c26ab485b66704689b9b8fbf1
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-08-19 19:39:00 +0200

    add new functions: render website, set all tasks done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 {
     let tasks = [];
     let hideDoneTasks = false;
+    const render = () => { 
+        renderTaskList();
+        renderButtons();
+        bindEvents();
+    };
     const renderTaskList = () => {
         let newPosition = "";
         for (const task of tasks) {
@@ -17,24 +22,22 @@
                 `;
         }
         document.querySelector(".js-section__taskList").innerHTML = newPosition;
-        bindEvents();
+        
     }
-    const isArrayEmpty = (array) => {
-        return array.length === 0 ? true : false;
-    };
-
     const renderButtons = () => {
         let arrayEmpty = isArrayEmpty(tasks);
         let allTasksDone = checkAllTasksDone(tasks);
         let addButtons =
             `
         <span class="section__header__text">Lista zadań</span>
-        <button class="${arrayEmpty === false ? "section__tasksInteractionButton" : "section__tasksInteractionButton--hidden"}">Ukryj ukończone</button>
-        <button ${allTasksDone === true ? "disabled" : ""} class="${arrayEmpty === false ? "section__tasksInteractionButton" : "section__tasksInteractionButton--hidden"}">Ukończ wszystkie</button>
+        <button class="js-section__toggleTasksViewButton ${arrayEmpty === false ? "section__tasksInteractionButton" : "section__tasksInteractionButton--hidden"}">Ukryj ukończone</button>
+        <button ${allTasksDone === true ? "disabled" : ""} class="js-section__setTasksDoneButton ${arrayEmpty === false ? "section__tasksInteractionButton" : "section__tasksInteractionButton--hidden"}">Ukończ wszystkie</button>
         `;
         document.querySelector(".js-section__header").innerHTML = addButtons;
     };
-
+    const isArrayEmpty = (array) => {
+        return array.length === 0 ? true : false;
+    };
     const checkAllTasksDone = (array) => {
         const filtrDoneTasks = tasks.filter(({ status }) => status);
         return array.length === filtrDoneTasks.length ? true : false;
@@ -48,8 +51,7 @@
                     ...tasks.slice(0, index),
                     ...tasks.slice(index + 1),
                 ];
-                renderTaskList();
-                renderButtons();
+                render();
             });
         });
         const statusTaskButtons = document.querySelectorAll(".js-section__taskList__checkedButton");
@@ -61,9 +63,25 @@
                     ...tasks.slice(index + 1),
 
                 ];
-                renderTaskList();
-                renderButtons();
+                render();
             });
+        });
+        const setTasksDoneButton = document.querySelector(".js-section__setTasksDoneButton");
+
+        setTasksDoneButton.addEventListener("click", () => {
+
+            tasks = tasks.map(obj => {
+                if (obj.status === false) {
+                    return { ...obj, status: true };
+                }
+                return obj;
+            });
+
+            render();
+        });
+        const toggleTasksViewButton = document.querySelector(".js-section__toggleTasksViewButton");
+        toggleTasksViewButton.addEventListener("click", () => {
+            console.log("CLICK1");
         });
     }
     const addNewTask = (newTask) => {
@@ -74,8 +92,7 @@
                 status: false,
             },
         ];
-        renderTaskList();
-        renderButtons();
+        render();
         resetformfield();
     }
     const focusTaskInput = () => {
@@ -88,7 +105,7 @@
     }
     const onFormSubmit = (event) => {
         event.preventDefault();
-        renderButtons();
+        render();
         const newTask = document.querySelector(".js-addTaskForm__addTaskInput").value.trim();
         if (newTask === "") {
             focusTaskInput();
@@ -101,4 +118,5 @@
         form.addEventListener("submit", onFormSubmit);
     };
     init();
+    
 }
